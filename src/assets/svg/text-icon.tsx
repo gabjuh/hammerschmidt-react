90cc869b46918c80309b7cc7b9b61f1db6f05503
gaborjuhasz <adgbe@yahoo.de>
2025-03-21 13:35:22 +0100

    add text icon

---
+++ src/assets/svg/text-icon.tsx
@@ -0,0 +1,13 @@
+const TextIcon = ({w, h}: {w: string, h: string}) => {
+  return (
+    <svg width={w} height={h} viewBox="0 0 48 48">
+        <g id="Q3_icons" data-name="Q3 icons" fill="none" stroke="#000" stroke-width="1" stroke-linecap="round">
+          <line x1="12" y1="10" x2="36" y2="10" />
+          <line x1="6" y1="18" x2="42" y2="18" />
+          <line x1="12" y1="26" x2="36" y2="26" />
+        </g>
+    </svg>
+  )
+}
+
+export default TextIcon
