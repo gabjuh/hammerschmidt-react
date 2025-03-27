488b8eccbdabc2bf6969e91853053eea64a72657
gaborjuhasz <adgbe@yahoo.de>
2025-03-27 11:51:46 +0100

    hide cards should not be shown

--- src/components/Musicians.tsx
+++ src/components/Musicians.tsx
@@ -10,8 +10,11 @@ const Musicians = () => {
     <div id="musicians" className="container mx-auto px-2 md:px-4 pt-14 xl:py-18 mb-0">
       <h2 className="text-center text-4xl font-bold mb-10 text-orange-300 font-title">Művészek</h2>
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-12 md:gap-6 relative">
-        {musiciansData.map((person, index) => (
-          <Person
+        {musiciansData.map((person, index) => {
+          if (person.showAsCard === false) {
+            return;
+          }
+          return <Person
             key={index}
             name={person.name}
             title={person.title}
@@ -22,7 +25,7 @@ const Musicians = () => {
             openedPersonDetailsId={openedPersonDetailsId}
             setOpenedPersonDetailsId={setOpenedPersonDetailsId}
           />
-        ))}
+        })}
       </div>
     </div>
   );
