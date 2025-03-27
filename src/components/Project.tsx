3ad2811b0bb53e3e85b1b43f6f94048f5d62cd57
gaborjuhasz <adgbe@yahoo.de>
2025-03-27 11:52:46 +0100

    bind in video, fix position issues

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,31 +1,95 @@
+import { useEffect, useRef } from 'react';
+import ReactMarkdown from 'react-markdown';
 import { Link } from 'react-router-dom';
+import rehypeRaw from 'rehype-raw';
 
-import projectImage from '../assets/img/hammerschmidt.png';
+import hammerschmidtVideo from '../assets/hammerschmidt.webm';
+import { musiciansData, musiciansDataT } from '../data/musicians-data';
+import { LinkRenderer } from '../pages/MusicianPortfolio';
 
 const Project = () => {
+  const containerRef = useRef<HTMLDivElement | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const hammerschmidt: musiciansDataT | undefined = musiciansData.find((person) => person.showAsCard === false);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        const video = videoRef.current;
+        if (!video) return;
+
+        if (entry.intersectionRatio === 1) {
+          // Teljesen látható → lejátszás
+          video.currentTime = 0;
+          video.play();
+        } else {
+          // Nem teljesen látható → megáll és visszaugrik
+          video.pause();
+          video.currentTime = 0;
+        }
+      },
+      {
+        threshold: 1.0, // Csak akkor játsszon, ha teljesen a képernyőn van
+      }
+    );
+
+    observer.observe(container);
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
-    <div id="hammerschmidt" className="container mx-auto flex flex-col-reverse items-center justify-center text-white xl:py-18 mt-10 md:mt-20 px-2 md:px-6">
+    <div
+      id="hammerschmidt"
+      className="container mx-auto flex flex-col-reverse items-center justify-center text-white xl:py-18 mt-10 md:mt-20 px-2 md:px-6"
+    >
       <div className="w-full bg-[#fff1] px-3 pt-10 md:p-8 rounded-lg items-center shadow-lg flex flex-wrap lg:flex-nowrap lg:gap-6 relative">
-        {/* Text Block (Left on Desktop, Below Image on Mobile) */}
-        <div className="w-full lg:w-1/2 text-center lg:text-right">
-          <p className="text-2xl font-title">A szerző</p>
-          <h2 className="font-title text-[2.4rem] leading-8.5 md:leading-10 sm:text-5xl my-5 text-orange-300 drop-shadow-lg">Andreas Hammerschmidt</h2>
-          <h3 className="text-3xl font-title sm:text-2xl mb-5 mt-3">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        {/* Text Block */}
+        <div id="project-markdown-text" className="w-full lg:w-1/2 text-center lg:text-right">
+          {/* <p className="text-2xl font-title">A szerző</p> */}
+          <h2 className="font-title text-[2.4rem] leading-8.5 md:leading-10 sm:text-5xl my-5 text-orange-300 drop-shadow-lg">
+            { hammerschmidt?.name }
+          </h2>
+          <h3 className="text-3xl font-title sm:text-2xl mb-5 mt-3 opacity-50">
+            „Zittaui Orfeusz, kinek hangjai Isten trónja előtt zengenek tovább.”
           </h3>
           <p className="font-content-extralight sm:text-base leading-6">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus rem sequi, facilis quos repellat culpa dicta, iusto nam quae nesciunt expedita dolores in dolorem veritatis totam, obcaecati deleniti porro quidem?
+            <ReactMarkdown rehypePlugins={[rehypeRaw]} components={{ a: LinkRenderer}}>
+              { hammerschmidt?.shortBio.replace(/\$\$/g, "\n\n") }
+            </ReactMarkdown>
           </p>
 
-          <Link to="/koncertprogram" className="inline-block font-content-medium mt-5 px-7 py-2 rounded-full bg-[#A14028] shadow-lg text-white font-bold hover:bg-[#A14028] transition duration-200">
+          <Link
+            to="/koncertprogram"
+            className="inline-block font-content-medium mt-5 px-7 py-2 rounded-full bg-[#A14028] shadow-lg text-white font-bold hover:bg-[#A14028] transition duration-200 mr-3"
+          >
             Koncertprogram
           </Link>
-
+          <Link
+            to="/portfolio/andreas-hammerschmidt"
+            className="inline-block font-content-medium mt-5 px-7 py-2 rounded-full bg-[#A14028] shadow-lg text-white font-bold hover:bg-[#A14028] transition duration-200"
+          >
+            Életrajz
+          </Link>
         </div>
 
-        {/* Image (Right on Desktop, Above on Mobile) */}
-        <div className="w-full lg:w-1/2 flex justify-center mb-8 md:mb-0 mt-10 lg:mt-0">
-          <img className="w-full max-w-[400px] md:max-w-[450px] rounded-xl shadow-lg" src={projectImage} alt="Hammerschmidt Image" />
+        {/* Video Block */}
+        <div
+          ref={containerRef}
+          className="w-full lg:w-1/2 flex justify-center mb-8 md:mb-0 mt-10 lg:mt-0"
+        >
+          <video
+            ref={videoRef}
+            className="w-full max-w-[400px] md:max-w-[450px] rounded-xl shadow-lg"
+            src={hammerschmidtVideo}
+            muted
+            loop={true}
+            playsInline
+          />
         </div>
       </div>
     </div>
