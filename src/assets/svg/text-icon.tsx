bd3a046b1e254ae8dc4b4e455904225e287d95b2
gaborjuhasz <adgbe@yahoo.de>
2025-04-07 15:27:17 +0200

    fix attr naming issue

--- src/assets/svg/text-icon.tsx
+++ src/assets/svg/text-icon.tsx
@@ -1,7 +1,7 @@
 const TextIcon = ({w, h}: {w: string, h: string}) => {
   return (
     <svg width={w} height={h} viewBox="0 0 48 48">
-        <g id="Q3_icons" data-name="Q3 icons" fill="none" stroke="#000" stroke-width="1" stroke-linecap="round">
+        <g id="Q3_icons" data-name="Q3 icons" fill="none" stroke="#000" strokeWidth="1" strokeLinecap="round">
           <line x1="12" y1="10" x2="36" y2="10" />
           <line x1="6" y1="18" x2="42" y2="18" />
           <line x1="12" y1="26" x2="36" y2="26" />
