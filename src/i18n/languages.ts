125e948ca566a2e8bb2441f9cbe54af63e142002
gaborjuhasz <adgbe@yahoo.de>
2025-04-15 06:25:30 +0200

    change language order

--- src/i18n/languages.ts
+++ src/i18n/languages.ts
@@ -1,7 +1,7 @@
 export const languages = {
+  hu: { label: 'Magyar', flag: '🇭🇺', code: 'hu' as LanguagesCodeT  },
   en: { label: 'English', flag: '🇬🇧', code: 'en' as LanguagesCodeT },
   de: { label: 'Deutsch', flag: '🇩🇪', code: 'de' as LanguagesCodeT  },
-  hu: { label: 'Magyar', flag: '🇭🇺', code: 'hu' as LanguagesCodeT  },
 };
 
 export type LanguagesCodeT = "hu" | "de" | "en";
