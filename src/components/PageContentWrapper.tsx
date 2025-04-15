aa757417d076817f38ce1d2e5159880f08937c92
gaborjuhasz <adgbe@yahoo.de>
2025-04-15 06:24:46 +0200

    remove margin double

--- src/components/PageContentWrapper.tsx
+++ src/components/PageContentWrapper.tsx
@@ -8,7 +8,7 @@ const PageContentWrapper = ({
   backTo?: string;
 } ) => {
   return (
-    <div id="privacy-policy" className="container mx-2 md:mx-auto min-h-[400px] px-3 py-7 md:p-6 bg-[#fff1] text-white my-10 shadow-lg rounded-lg relative">
+    <div id="privacy-policy" className="container mx-auto min-h-[400px] px-3 py-7 md:p-6 bg-[#fff1] text-white my-10 shadow-lg rounded-lg relative">
       {children}
       {backTo && <BackLink target={backTo} />}
     </div>
